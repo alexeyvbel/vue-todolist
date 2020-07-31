10ecab16f2339b33352a0053be0ff12bf82f0870
AlexJ <alexeyvbel@gmail.com>
2020-07-31 15:12:07 +0700

    create-modal-window

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
     <div class="todo-wrapper">
       <div class="todo-container">
         <todo-list :todos="todos"/>
+        <div class="todo-create-btn-container">
+          <div class="app-button">
+            Create
+          </div>
+        </div>
+      </div>
+
+      <div class="modal">
+        <div class="modal-content">
+          Modal window
+        </div>
       </div>
     </div>
   </div>
@@ -52,6 +63,35 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app-button {
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #795899;
+  color: white;
+  font-weight: bold;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content{
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
 }
 
 .todo {
